fb13db3b6b21819a369fba2f319ebb538251f7a0
agustinaov <aguscovalle@gmail.com>
2025-03-07 17:17:50 -0300

    Corrigiendo configuración de rutas para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/mi-organizador/",  // 👈 Asegura que coincida con el nombre de tu repo en GitHub
+  base: "/mi-organizador/",  // 👈 Asegura que el nombre coincide con el repo en GitHub
   plugins: [react()],
 });
 
